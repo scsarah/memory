d830af1ed9b2e6870b286b97a43fba02441dee4a
SarahSC <sarah.curbet@gmail.com>
2019-08-14 15:05:29 +0200

    show and hide cards according to memory rules

--- js/memory.js
+++ js/memory.js
@@ -19,16 +19,42 @@ function getRandomMemoryCards() {
     } else {
       imgNumber = elt - 8;
     }
-    html += '<div id="ia__memory__card' + elt + '" class="ia__memory__block ia__memory__card' + imgNumber + '" >' + '<img src="img/img' + imgNumber + '.jpg" alt="a feline" class="ia__memory__img"></img>' + ' </div> ';
+    html += '<div class="ia__memory__card" >' + '<img src="img/img' + imgNumber + '.jpg" alt="a feline" class="ia__memory__img"></img>' + ' </div> ';
   });
   return html;
 }
 
 $(document).ready(function () {
-  html = getRandomMemoryCards();
-  $(".ia__memory__container").append(html);
+  $(".ia__memory__container").append(getRandomMemoryCards());
 
-  $(".ia__memory__block").click(function () {
-    $(this).children().show();
+  $card = $(".ia__memory__card");
+
+  count = 0;
+  cardsRound = []
+
+  $card.click(function () {
+    $this = $(this);
+    if (!($this.hasClass("ia__memory__clicked") && $this.hasClass("ia__memory__found"))) {
+
+      if (count < 2) {
+        $this.addClass("ia__memory__clicked");
+        cardsRound.push($this.children().attr("src"));
+        console.log(cardsRound);
+        count += 1;
+        console.log(count);
+
+      } else if (count === 2) {
+        if (cardsRound[0] === cardsRound[1]) {
+          $(".ia__memory__clicked").addClass("ia__memory__found");
+        } else {
+          $card.delay(1000).removeClass("ia__memory__clicked");
+        }
+        cardsRound = [];
+        count = 0;
+      }
+    }
   });
+
+
+
 });
