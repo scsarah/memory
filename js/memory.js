e7a3d33565892b7c9a7560cd67524ad07c22d920
SarahSC <sarah.curbet@gmail.com>
2019-08-15 19:15:40 +0200

    Improvement of the game design + doc

--- js/memory.js
+++ js/memory.js
@@ -1,7 +1,12 @@
-function getRandomMemoryArray() {
+/**
+ * Create an array with random numbers.
+ * @param {Number} nb The array's length needed.
+ * @returns {Array} The random array created.
+ */
+function getRandomMemoryArray(nb) {
   let memoryArray = [];
-  while (memoryArray.length < 16) {
-    card = Math.floor(Math.random() * (16 - 1 + 1)) + 1;
+  while (memoryArray.length < nb) {
+    card = Math.floor(Math.random() * (nb - 1 + 1)) + 1;
     if (!(memoryArray.includes(card))) {
       memoryArray.push(card);
     }
@@ -9,8 +14,13 @@ function getRandomMemoryArray() {
   return memoryArray;
 }
 
+/**
+ * Create a string that represents the HTML content of the memory's cards shuffled.
+ * 
+ * @returns {string} A string that represents the HTML content of the memory's cards.
+ */
 function getRandomMemoryCards() {
-  let memoryArray = getRandomMemoryArray();
+  let memoryArray = getRandomMemoryArray(16);
   let html = "";
   let imgNumber = 0;
   memoryArray.forEach(function (elt, i) {
@@ -24,10 +34,12 @@ function getRandomMemoryCards() {
   return html;
 }
 
+// Run the Game.
 document.body.onload = runGame();
 
 function runGame() {
   $(document).ready(function () {
+
     // Display a random memory game
     let $memoryContainer = $(".ia__memory__container")
     $($memoryContainer).append(getRandomMemoryCards());
@@ -35,19 +47,21 @@ function runGame() {
     // Declare useful variables
     let $cards = $(".ia__memory__card");
     let $lapCounter = $(".ia__memory__lap__counter");
-
     let cardPerLap = 0;
     let lap = 0;
     let cardsLapImgs = [];
     let cardsHaveClassFound = [];
 
+    // Listen when and which card is clicked
     $cards.click(function () {
       $this = $(this);
+
+      // Verify that the card has not already been returned
       if (!($this.hasClass("ia__memory__clicked")) && !($this.hasClass("ia__memory__found"))) {
 
         if (cardPerLap < 2) {
 
-          // Identifie this turn's cards and their images
+          // Identify this lap's cards and their images
           $this.addClass("ia__memory__clicked");
           cardsLapImgs.push($this.children().attr("src"));
           cardPerLap += 1;
@@ -60,19 +74,19 @@ function runGame() {
 
             setTimeout(function () {
 
-              // Check if images of both card are similar .
+              // Check if the images of both card are similar.
               if (cardsLapImgs[0] === cardsLapImgs[1]) {
-                // Add the class "ia__memory__found" to both cards.
+
+                // Add the class "ia__memory__found" to both cards when images are the same.
                 $(".ia__memory__clicked").addClass("ia__memory__found");
 
-                // Check if all the cards have been found.
+                // Check if all cards have been found.
                 for (let i = 1; i <= $cards.length; i++) {
                   cardsHaveClassFound.push($('#ia__memory__card__' + i).hasClass("ia__memory__found"));
                 }
 
                 // Check when the game is finished and display it.
                 if (cardsHaveClassFound.includes(false) === false) {
-                  console.log("end game");
                   $(".ia__memory__result").text("Bravo, vous avez terminé le memory en " + lap + " coups !")
                   $(".ia__memory__end__game__background").css("display", "flex");
                 }
@@ -90,15 +104,13 @@ function runGame() {
       }
     });
 
+    // Reinitialize the game.
     $(".ia__memory__restart").click(function () {
       $cards.remove();
       $(".ia__memory__end__game__background").css("display", "none");
 
       $lap = 0;
       $lapCounter.text("Coups : 0");
-
-      // $memoryContainer.append(getRandomMemoryCards());
-      // $cards = $(".ia__memory__card");
     });
 
   });
