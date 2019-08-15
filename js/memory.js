0b9c3dd317e61e2cbb2fb41f53436a7516a511be
SarahSC <sarah.curbet@gmail.com>
2019-08-15 12:06:16 +0200

    Detect when the game is finished

--- js/memory.js
+++ js/memory.js
@@ -19,44 +19,74 @@ function getRandomMemoryCards() {
     } else {
       imgNumber = elt - 8;
     }
-    html += '<div class="ia__memory__card" >' + '<img src="img/img' + imgNumber + '.jpg" alt="a feline" class="ia__memory__img"></img>' + ' </div> ';
+    html += '<div id="ia__memory__card__' + elt + '" class="ia__memory__card" >' + '<img src="img/img' + imgNumber + '.jpg" alt="a feline" class="ia__memory__img"></img>' + ' </div> ';
   });
   return html;
 }
 
 $(document).ready(function () {
+  // Display a random memory game
   $(".ia__memory__container").append(getRandomMemoryCards());
 
-  let $card = $(".ia__memory__card");
+  // Declare useful variables
+  let $cards = $(".ia__memory__card");
   let $lapCounter = $(".ia__memory__lap__counter");
 
-  let count = 0;
-  let round = 0;
-  let cardsRound = []
+  let cardPerLap = 0;
+  let lap = 0;
+  let cardsLapImgs = [];
+  let cardsHaveClassFound = [];
 
-  $card.click(function () {
+
+  $cards.click(function () {
     $this = $(this);
+
     if (!($this.hasClass("ia__memory__clicked")) && !($this.hasClass("ia__memory__found"))) {
-      if (count < 2) {
+
+      if (cardPerLap < 2) {
+
+        // Identifie this turn's cards and their images
         $this.addClass("ia__memory__clicked");
-        cardsRound.push($this.children().attr("src"));
-        count += 1;
-        if (count === 2) {
-          round++;
-          $lapCounter.text("Coups : " + round);
+        cardsLapImgs.push($this.children().attr("src"));
+        cardPerLap += 1;
+
+        if (cardPerLap === 2) {
+
+          // Count the number of laps.
+          lap++;
+          $lapCounter.text("Coups : " + lap);
+
           setTimeout(function () {
-            if (cardsRound[0] === cardsRound[1]) {
+
+            // Check if images of both card are similar .
+            if (cardsLapImgs[0] === cardsLapImgs[1]) {
+              // Add the class "ia__memory__found" to both cards.
               $(".ia__memory__clicked").addClass("ia__memory__found");
-            } // else {
-            $card.removeClass("ia__memory__clicked");
-            // }
-            count = 0;
-            cardsRound = [];
+
+              // Check if all the cards have been found.
+              for (let i = 1; i <= $cards.length; i++) {
+                cardsHaveClassFound.push($('#ia__memory__card__' + i).hasClass("ia__memory__found"));
+              }
+
+              // Check when the game is finished and display it.
+              if (cardsHaveClassFound.includes(false) === false) {
+                alert("Bravo, vous avez terminé le jeu en " + lap + " coups.");
+              }
+            }
+
+            // reset all variables for next lap.
+            $cards.removeClass("ia__memory__clicked");
+            cardPerLap = 0;
+            cardsLapImgs = [];
+            cardsHaveClassFound = []
+
           }, 700);
         }
       }
     }
   });
+
+
 
 
 
